9e2a4a84a96868b45969850968ec717e4e6c1a27
hsinjungLee <144487630+hsinjungLee@users.noreply.github.com>
2023-10-09 15:41:59 -0400

    complete part of them but still have some problem.

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -15,7 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         // You can test your helper functions by  calling them from onCreate() and
         // printing their output to the Log, which is visible in the LogCat:
-        // eg. Log.d("function output", getTestDataArray().toString())
+        Log.d("function output", getTestDataArray().toString())
+        Log.d("function output", testData.toString())
+
+
+        val numbers = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
+        if (averageLessThanMedian(numbers)) {
+            Log.d("Comparison", "Average is less than the median.")
+        } else {
+            Log.d("Comparison", "Average is greater than or equal to the median.")
+        }
+
 
     }
 
@@ -28,10 +38,13 @@ class MainActivity : AppCompatActivity() {
 
     // Return a list of random, sorted integers
     private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
+        val testArray = MutableList(10){Random.nextInt()}.apply { sort() }
         testArray.sort()
         return testArray
     }
+    val testData = getTestDataArray()
+
+
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
@@ -43,7 +56,10 @@ class MainActivity : AppCompatActivity() {
             sortedList[sortedList.size / 2]
 
         return avg < median
+
     }
+
+
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
     private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
@@ -55,11 +71,20 @@ class MainActivity : AppCompatActivity() {
             textView = TextView(context)
             textView.setPadding(5, 10, 10, 0)
             textView.textSize = 22f
+
+            val position = 0
+            val recycledView: View? = null
+            val collection = listOf(1, 2, 3, 4, 5)
+            val context = this
+
+            val view = getView(position, recycledView, collection, context)
         }
 
         textView.text = collection[position].toString()
 
         return textView
+
     }
+
 
 }
